5a891f2ce4432ec9e34839d57973cff152d12658
Mackenzie McIntyre <362574782@GoTVDSB.ca>
2026-05-25 13:15:18 -0400

    figure out the boy and glitch to living room

--- Game1.cs
+++ Game1.cs
@@ -10,8 +10,24 @@ namespace final_project_rough_draft__Stardew_
         private SpriteBatch _spriteBatch;
 
 
-        Texture2D livingRoomTexture, bedRoomTexture, pianoRoomTexture;
-        Rectangle window;
+        Texture2D livingRoomTexture, bedRoomTexture, pianoRoomTexture, pixelboyTexture;
+
+
+        Rectangle window, livingRoomDoor, bedRoomDoor, pianoRoomDoor, pixelboyRect;
+        MouseState mouseState;
+        Vector2 cursorPosition;
+       
+
+        enum Screen
+        {
+            Intro,
+            livingRoom,
+            bedRoom,
+            pianoRoom,
+            End
+        }
+        Screen screen;
+
 
         public Game1()
         {
@@ -25,9 +41,16 @@ namespace final_project_rough_draft__Stardew_
             // TODO: Add your initialization logic here
 
             window = new Rectangle(0, 0, 800, 600);
+            livingRoomDoor = new Rectangle(216, 542, 253, 264);
+            bedRoomDoor = new Rectangle(397, 536, 1065, 909);
+            pianoRoomDoor = new Rectangle(764, 319, 461, 440);
+            pixelboyRect = new Rectangle(800, 600, 10, 10);
+
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
+
+            screen = Screen.livingRoom;
 
 
             base.Initialize();
@@ -38,6 +61,10 @@ namespace final_project_rough_draft__Stardew_
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             livingRoomTexture = Content.Load<Texture2D>("livingRoom2");
+            bedRoomTexture = Content.Load<Texture2D>("bedRoom2");
+            pianoRoomTexture = Content.Load<Texture2D>("pianoRoom2");
+            pixelboyTexture = Content.Load<Texture2D>("pixelboy");
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -47,23 +74,95 @@ namespace final_project_rough_draft__Stardew_
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            this.Window.Title = mouseState.Position.ToString();
+            mouseState = Mouse.GetState();
+            cursorPosition = new Vector2(mouseState.X, mouseState.Y);
+            if (screen == Screen.livingRoom)
+            {
 
-            base.Update(gameTime);
+                if  (mouseState.RightButton == ButtonState.Pressed && livingRoomDoor.Contains(mouseState.Position))
+                {
+                    screen = Screen.bedRoom;
+
+
+                }
+                if (mouseState.LeftButton == ButtonState.Pressed && livingRoomDoor.Contains(mouseState.Position))
+                {
+                    screen = Screen.pianoRoom;
+
+
+                }
+
+
+
+            }
+
+            else if (screen == Screen.pianoRoom)
+            {
+                if (mouseState.RightButton == ButtonState.Pressed && pianoRoomDoor.Contains(mouseState.Position))
+                {
+                    screen = Screen.bedRoom;
+
+
+                }
+                if (mouseState.LeftButton == ButtonState.Pressed && pianoRoomDoor.Contains(mouseState.Position))
+                {
+                    screen = Screen.livingRoom;
+
+
+                }
+            }
+            else if (screen == Screen.bedRoom)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed && bedRoomDoor.Contains(mouseState.Position))
+                {
+                    screen = Screen.pianoRoom;
+
+
+                }
+                if (mouseState.RightButton == ButtonState.Pressed && bedRoomDoor.Contains(mouseState.Position))
+                {
+                    screen = Screen.livingRoom;
+
+
+                }
+            }
+
+                // TODO: Add your update logic here
+
+                base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
 
             _spriteBatch.Begin();   
 
-            _spriteBatch.Draw(livingRoomTexture, window, Color.White);
+            
+            if (screen == Screen.livingRoom)
+            {
+                _spriteBatch.Draw(livingRoomTexture, window, Color.White);
+                _spriteBatch.Draw (pixelboyTexture, livingRoomDoor, Color.White);
+            }
 
-            _spriteBatch.End();
+            else if (screen == Screen.bedRoom)
+            {
+                _spriteBatch.Draw(bedRoomTexture, window, Color.White);
+                _spriteBatch.Draw(pixelboyTexture, bedRoomDoor, Color.White);
+            }
+            else if (screen == Screen.pianoRoom)
+            {
+                _spriteBatch.Draw(pianoRoomTexture, window, Color.White);
+                _spriteBatch.Draw(pixelboyTexture, pianoRoomDoor, Color.White);
+            }
+
+
+
+                _spriteBatch.End();
 
             base.Draw(gameTime);
         }
